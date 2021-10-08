9230792cc5fb1af66b456cbf9dad8d27739e2550
Masanari Suzuki <ma_yamaki@yahoo.com>
2021-10-08 12:53:32 +0900

    befor less code

--- s_ren/polls/views.py
+++ s_ren/polls/views.py
@@ -39,12 +39,12 @@ def vote(request, question_id):
     # return HttpResponse("You're voting on question %s" % question_id)
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['Choice'])
-    except (KyeError, Choice.DoesNotExist):
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html',
                         {'question': question,
                         'error_message': "You didnt select a choice."})
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', arge=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
