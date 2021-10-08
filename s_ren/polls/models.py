ac44e0577a2b0938884040e139ada0cdffd23454
Masanari Suzuki <ma_yamaki@yahoo.com>
2021-10-08 09:33:58 +0900

    git ignore

--- s_ren/polls/models.py
+++ s_ren/polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    """docstring for Question."""
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+#models.model    def __init__(self, arg):
+        #super(Question, models.model).__init__()
+        #question_text = models.CharFiel
+        #self.arg = arg
+        #question_text = models.CharFiel
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
