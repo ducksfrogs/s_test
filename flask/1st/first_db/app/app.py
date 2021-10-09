e11e2913349a4bdd7d3a692559e3b69d3b290107
ducksfrogs <ma_yamaki@yahoo.com>
2021-10-09 20:01:07 +0900

    fin db

--- flask/1st/first_db/app/app.py
+++ flask/1st/first_db/app/app.py
@@ -1,34 +1,22 @@
 from flask import Flask, render_template, request
-import psycopg2
-from psycopg2._psycopg import *
+
+from models.models import OnegaiContent
 
 app = Flask(__name__)
 
 
-#users = 'postgres'
-#dbname = 'test2'
-#password = '0310'
-#conn = psycopg2.connect(" user="+users+" dbname="+dbname+" password="+password)
-#cur = conn.cursor()
-#cur.execute('select * from actor')
-#results = cur.fetchall()
-
 @app.route("/")
-# def hello():
-    # return "Hello world"
-
-
 @app.route("/index")
 def index():
     name = request.args.get("name")
-    animals = ['panda', 'coara', 'rakko', 'cat']
-    return render_template("index.html", name=name, animals=animals)
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html", name=name, all_onegai=all_onegai)
 
 @app.route("/index",methods=["post"])
 def post():
     name = request.form["name"]
-    animals = ['panda', 'coara', 'rakko', 'cat']
-    return render_template("index.html", name=name, animals=animals)
+    all_onegai= OnegaiContent.query.all()
+    return render_template("index.html", name=name, all_onegai=all_onegai)
 
 
 
