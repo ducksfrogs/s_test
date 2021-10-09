9837f7342ec2cae85ca708826f7f2ea027ca2d0b
ducksfrogs <ma_yamaki@yahoo.com>
2021-10-09 20:23:22 +0900

    d

--- flask/1st/first_db/app/app.py
+++ flask/1st/first_db/app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
-
 from models.models import OnegaiContent
+
+from models.database import db_session
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -18,6 +20,14 @@ def post():
     all_onegai= OnegaiContent.query.all()
     return render_template("index.html", name=name, all_onegai=all_onegai)
 
+@app.route("/add", methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = OnegaiContent(title, body, datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return index()
 
 
 if __name__=="__main__":
