aee4dd6e088bd8eb99b4515d6b180d0d43c76718
Masanari Suzuki <ma_yamaki@yahoo.com>
2021-10-11 11:44:50 +0900

    11hi gozen fin

--- flask/1st_pg/first_db/app/app.py
+++ flask/1st_pg/first_db/app/app.py
@@ -13,12 +13,13 @@ def index():
     all_cities = Weather.query.all()
     return render_template("index.html", name=name, all_cities=all_cities)
 
-@app.route("/index",methods=["post"])
-def post():
-    name = request.form["name"]
-    all_cities= Weather.all()
-    return render_template("index.html", name=name, all_cities=all_cities)
-
+@app.route("/searchCity", methods=["post"])
+def searchCity():
+    city = request.form["city"]
+    cityRq = db_session.query(Weather.city,
+                            Weather.temp_hi,
+                            Weather.temp_lo).filter(Weather.city==city).all()
+    return render_template("searchCity.html", all_cities=cityRq)
 
 if __name__=="__main__":
     app.run(dubug=True)
